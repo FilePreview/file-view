16a628d45807c103028b2981ed59d61a4c7ae20a
Rain7_osu <1789446861@qq.com>
2020-12-19 00:07:50 +0800

    upload file

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,46 @@
 import React, { Component } from 'react'
-import { Breadcrumb, Button } from 'antd'
+import { Breadcrumb, Button, Input, Modal, Upload, message } from 'antd'
 import './Navbar.css'
+import { UploadOutlined } from '@ant-design/icons'
 import { connect } from 'react-redux'
 import { backAsync, getFileListAsync } from '../redux/actions'
 import { Path } from '../Path'
 
+const props = {
+  name: 'file',
+  action: '/fileUpload',
+  headers: {
+    authorization: 'authorization-text',
+  },
+  onChange (info) {
+    if (info.file.status !== 'uploading') {
+      console.log(info.file, info.fileList)
+    }
+    if (info.file.status === 'done') {
+      message.success(`${info.file.name} 上传成功`)
+    } else if (info.file.status === 'error') {
+      message.error(`${info.file.name} 上传失败.`)
+    }
+  },
+}
+
 class Navbar extends Component {
 
-  handleBack = ()=> {
+  state = {
+    showModal: false
+  }
+
+  handleBack = () => {
     let path = this.props.path
     this.props.backAsync(path)
+  }
+
+  handleUpload = () => {
+    this.setState({ showModal: true })
+  }
+
+  handleCancel = () => {
+    this.setState({ showModal: false })
   }
 
   render () {
@@ -30,7 +61,13 @@ class Navbar extends Component {
           </div>
         </div>
         <div className="right">
+          <Button onClick={this.handleUpload}>上传文件</Button>
         </div>
+        <Modal visible={this.state.showModal} onCancel={this.handleCancel} footer={null}>
+          <Upload {...props}>
+            <Button icon={<UploadOutlined/>}>点击上传</Button>
+          </Upload>
+        </Modal>
       </div>
     )
   }
